78074518eb9a46205f36f20480ba5c739b458447
Alex Coleman <alexjcoleman@gmail.com>
2023-01-26 16:00:17 +0000

    added some early API planning

---
+++ R/food_stuffs.R
@@ -0,0 +1,17 @@
+
+# class BaseProduct
+# expects default product attributes
+
+# class Food(BaseProduct)
+# differs depending on units g/ml
+
+# class Drink(BaseProduct)
+# differs on drink.format
+
+# class ReadyDrink(Drink)
+
+# class PowderedDrink(Drink)
+# needs volume and weight
+
+# class CordialDrink(Drink)
+# needs volume and weight
