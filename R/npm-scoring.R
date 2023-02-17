36e51ad61045a4cfaba612e83a376a51951310d1
Sparrow0hawk <alexjcoleman@gmail.com>
2023-02-17 20:13:29 +0000

    refactor scoring logic into more generic function

--- R/npm-scoring.R
+++ R/npm-scoring.R
@@ -1,4 +1,51 @@
-NPM_a_score <- function(row) {
+# define threshold constants
+A_SCORE_THRESHOLDS <- c(3350, 3015, 2680, 2345, 2010, 1675, 1340, 1005, 670, 335)
+SUGAR_SCORE_THRESHOLDS <- c(45, 40, 36, 31, 27, 22.5, 18, 13.5, 9, 4.5)
+
+
+#' a generic NPM scoring dispatcher
+#'
+#'
+NPM_score_function <- function(row, type) {
+    stopifnot(
+        "The passed type to NPM_score_function does not match expected types " =
+            type %in% c("a", "sugar")
+    )
+
+    score <- switch(tolower(type),
+        "a" = scoring_function(a_value_adjuster(row), A_SCORE_THRESHOLDS),
+        "sugar" = scoring_function(sugar_adjuster(row), SUGAR_SCORE_THRESHOLDS),
+        stop(paste0(
+            "NPM_score_function can't determine thresholds type from ",
+            type, " that has been passed"
+        ))
+    )
+
+    return(score)
+}
+
+#' a template scoring function
+#'
+#' @param value a passed numeric value
+#' @param thresholds a vector of 10 thresholds to use to score against
+scoring_function <- function(value, thresholds) {
+    stopifnot("thresholds has no length" = length(thresholds) > 1)
+    # TODO: linter warning to use seq_along
+    for (x in rev(1:length(thresholds))) {
+        if (value > thresholds[x]) {
+            # print(paste0(value, " > ", thresholds[x]))
+            score <- x
+            break
+        } else if (x != 1) {
+            next
+        } else {
+            score <- 0
+        }
+    }
+    return(score)
+}
+
+a_value_adjuster <- function(row) {
     stopifnot(
         "The passed data to NPM_a_score does not have the required columns" =
             c("energy_measurement_kj", "energy_measurement_kcal") %in% names(row)
@@ -10,32 +57,23 @@ NPM_a_score <- function(row) {
         ((row["energy_measurement_kcal"] / row["sg_adjusted_weight"]) * 100) * 4.184
     }
 
-    return(a_scorer(a_value))
+    return(a_value)
 }
 
-a_scorer <- function(value) {
-    score <- if (value > 3350) {
-        10
-    } else if (value > 3015) {
-        9
-    } else if (value > 2680) {
-        8
-    } else if (value > 2345) {
-        7
-    } else if (value > 2010) {
-        6
-    } else if (value > 1675) {
-        5
-    } else if (value > 1340) {
-        4
-    } else if (value > 1005) {
-        3
-    } else if (value > 670) {
-        2
-    } else if (value > 335) {
-        1
-    } else {
-        0
-    }
-    return(score)
+#' Function for calculating score for sugar content
+#'
+#' @param row a row in a dataframe containing
+#'  "sg_adjusted_measurement" and "sugar_measurement_g" columns
+#' @return a value from 1 to 10
+sugar_adjuster <- function(row) {
+    # could this `sg_adjusted_weight` column be an argument to be more flexible
+    stopifnot(
+        "The passed data to NPM_sugar_score does not have the required columns" =
+            c("sg_adjusted_weight", "sugar_measurement_g") %in% names(row)
+    )
+
+    sugar_adjusted <- (row["sugar_measurement_g"]
+    / row["sg_adjusted_weight"]) * 100
+
+    return(sugar_adjusted)
 }
