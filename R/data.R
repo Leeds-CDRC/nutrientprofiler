caf0ba3cd4f4eedb550c41c862e8f3aec2901ba6
Sparrow0hawk <alexjcoleman@gmail.com>
2023-04-23 09:15:20 +0000

    add an example dataset

---
+++ R/data.R
@@ -0,0 +1,32 @@
+#' Example dataset called CDRC Drinks
+#'
+#' A handcrafted set of data that can be used as the input for
+#' nutrientprofiler functions
+#'
+#' @format `cdrcdrinks` ##
+#' A data frame of 10 rows and 21 columns:
+#' \describe{
+#'   \item{name}{Name of product}
+#'   \item{brand}{Brand of product}
+#'   \item{product_category}{Category of product}
+#'   \item{product_type}{Type of product, i.e. Food or Drink}
+#'   \item{food_type}{Type of food, used for specific gravity conversion}
+#'   \item{drink_format}{Format of drink, used for specific gravity conversion}
+#'   \item{drink_type}{Type of drink, used for speciifc gravity conversion}
+#'   \item{nutrition_info}{How nutritional information is presented, for specific gravity conversion}
+#'   \item{energy_measurement_kj}{Energy measurement in KJ}
+#'   \item{energy_measurement_kcal}{Energy measurement in kcal}
+#'   \item{sugar_measurement_g}{Sugar content measured in in grams}
+#'   \item{fat_measurement_g}{The fat content measured in grams}
+#'   \item{salt_measurement_g}{The salt content measured in grams}
+#'   \item{sodium_measurement_mg}{The salt content measured in mg of sodium}
+#'   \item{fibre_measurement_nsp}{The fibtre measurement using NSP}
+#'   \item{fibre_measurement_aoac}{The fibre measurement using AOAC}
+#'   \item{protein_measurement_g}{The protein emasurement of the product}
+#'   \item{fruit_nut_measurement_percent}{The percent of fruit and nuts in the product}
+#'   \item{weight_g}{The mass of the product if solid}
+#'   \item{volume_ml}{The volume of the product if liquid}
+#'   \item{volume_water_ml}{The volume of water for diluted products}
+#' }
+#'
+"cdrcdrinks"
