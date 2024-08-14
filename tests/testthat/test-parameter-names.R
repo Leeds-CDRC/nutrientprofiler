59e97a1c5bf6cf0ca3dec5797bcb76f608e5dc66
Maeve Murphy Quinlan <mmurphyquinlan@gmail.com>
2024-08-14 16:41:36 +0100

    Add further parameter naming tests

--- tests/testthat/test-parameter-names.R
+++ tests/testthat/test-parameter-names.R
@@ -10,8 +10,15 @@ test_that("inputDataCheck assesses parameter names correctly", {
     expect_output(inputDataCheck(test_data),"All required column names found. Proceed with analysis.")
 })
 
-test_that("inputDataCheck find missing parameter names", {
+test_that("inputDataCheck finds missing parameter names", {
     expect_output(inputDataCheck(test_data_incomplete),"The provided dataframe is missing the following required column names:")
 })
 
+test_that("parameterRename replaces missing parameter names", {
+    expect_output(parameterRename(missing_column_name = 'satfat_measurement_g', associated_data_column = "fat_measurement_g", data_frame = test_data_incomplete),"Replacing fat_measurement_g with satfat_measurement_g")
+})
 
+test_that("listMissingParameters lists missing parameter names", {
+    expect_output(print(listMissingParameters(test_data_incomplete)), 
+    "satfat_measurement_g")
+})
